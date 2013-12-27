b1c97ed18375860c23d5263898591c32ab8aedd6
Radoslaw Przybyl <Radoslaw.Przybyl@sabre.com>
2013-12-27 22:14:24 +0100

    added timer to measure download time

--- czytnik/src/main/java/com/czytnik/com/RSSFeedManager.java
+++ czytnik/src/main/java/com/czytnik/com/RSSFeedManager.java
@@ -1,6 +1,8 @@
 package com.czytnik.com;
 
 
+import android.util.Log;
+
 import java.util.List;
 import java.util.Vector;
 
@@ -13,7 +15,25 @@ public class RSSFeedManager {
         sites.add("http://www.sportowefakty.pl/koszykowka/index.rss");
         sites.add("http://www.sportowefakty.pl/moto/index.rss");
 
+
+        long starTime = System.currentTimeMillis();
         loadSites();
+
+        long endTime = System.currentTimeMillis() - starTime;
+        time(endTime);
+
+    }
+
+    private void time(long interval) {
+        int tens = (int) interval;
+        int seconds = (int) interval / 1000;
+        int minutes = seconds / 60;
+        int hours = minutes / 60;
+        tens = tens % 10;
+        seconds = seconds % 60;
+
+        String time = String.format("%d:%02d:%02d.%d", hours, minutes, seconds, tens);
+        Log.d("DUPA", time);
     }
 
     public List<RSSFeed> getFeeds() {
